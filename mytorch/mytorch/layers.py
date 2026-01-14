f7049d8c732e46afc0a990b9e4654cd332ca91d3
zhongqi <zhongqi2@andrew.cmu.edu>
2026-01-14 17:09:58 -0500

    release HW1-1

---
+++ mytorch/mytorch/layers.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# ============================================================
+# Linear function: f(x, W) = W x
+# ============================================================
+def linear(x, W):
+    """Linear function f(W) = Wx."""
+    
+    return W @ x
+
+
+def linear_lmap(x, W, dx=None):
+    """Differential of linear map at x along dx."""
+    
+    return None
+
+def linear_lmap_adjoint(x, W, u):
+    """Adjoint differential of linear map at x along output direction u."""
+
+    return None
---
+++ mytorch/tests/test_finite_diff.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+from mytorch.layers import (
+    linear,
+    linear_lmap,
+    linear_lmap_adjoint,
+)
+
+
+# ============================================================
+# Linear function finite diff test: f(x, W) = W x
+# ============================================================
+def _test_linear_lmap(f, f_lmap):
+    """Test the linear map using finite differences."""
+
+    lmap_exact = None
+    lmap_finite_diff = None
+    
+    np.testing.assert_allclose(lmap_exact, lmap_finite_diff, rtol=1e-5, atol=1e-8)
+
+
+def _test_linear_lmap_adjoint(f, f_lmap_adjoint):
+    """Test the adjoint map using finite differences."""
+
+    lmap_adjoint_exact = None
+    lmap_adjoint_finite_diff = None
+
+    np.testing.assert_allclose(
+        lmap_adjoint_exact, lmap_adjoint_finite_diff, rtol=1e-5, atol=1e-8
+    )
+
+# ============================================================
+# Run all tests
+# ============================================================
+def test_lmaps():
+    for i in range(10):
+        np.random.seed(i)
+        _test_linear_lmap(linear, linear_lmap)
+
+
+def test_lmaps_adjoint():
+    for i in range(10):
+        np.random.seed(i)
+        _test_linear_lmap_adjoint(linear, linear_lmap_adjoint)
+
+
+if __name__ == "__main__":
+    test_lmaps()
+    test_lmaps_adjoint()
